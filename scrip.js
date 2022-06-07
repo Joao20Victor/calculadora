9fcefdeefc69c3e7b864fcf9a13e91fac6f40474
Joao20Victor <joao.super.sayajin.007@gmail.com>
2022-06-07 15:33:11 -0300

    Calculadora pronta

---
+++ scrip.js
@@ -0,0 +1,24 @@
+function inserir(num){
+var numero = document.getElementById('result').innerHTML 
+document.getElementById('result').innerHTML = numero + num
+
+
+}
+function limpar(){
+    document.getElementById('result').innerHTML = "";
+}
+function apagar(){
+   var resultado = document.getElementById('result').innerHTML;
+   document.getElementById('result').innerHTML = resultado.substring(0, resultado.length -1)
+   
+
+  
+    
+}
+function calcular(){
+    var resultado = document.getElementById("result").innerHTML;
+    if(resultado){
+        document.getElementById("result").innerHTML = eval(resultado);
+        
+}
+}
